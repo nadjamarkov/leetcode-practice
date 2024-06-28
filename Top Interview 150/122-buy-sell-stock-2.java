a59e3e40218314ffaf8bbaf3e0519edf5b80fa60
Nadja Markov <61389806+nadjamarkov@users.noreply.github.com>
2024-06-28 11:27:04 -0400

    Create 122-buy-sell-stock-2.java

---
+++ Top Interview 150/122-buy-sell-stock-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buyingPrice = prices[0];
+        int profit = 0;
+        for(int i = 1; i<prices.length; i++){
+            if(buyingPrice<prices[i]){
+                profit += prices[i] - buyingPrice;
+            }
+            buyingPrice = prices[i];
+        }
+        return profit;
+    }
+}
